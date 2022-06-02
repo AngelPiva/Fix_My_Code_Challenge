cae6acfb847cf541395d299a4e017e10dcea2917
AngelPiva <AngelPiva@gmail.com>
2022-06-02 20:33:51 +0000

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """
-module that define a class Square
+module that defines class Square
 """
 
 
 class Square():
-    """class square"""
+    """ clas square """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """__init__"""
+        """ init method """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.height * self.width
 
     def permiter_of_my_square(self):
-        """ Perimeter of the square """
+        """ perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string representation"""
+        """ string representation"""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
-    """checks if __name__ == '__main__'"""
+    """ checks if __name__ == '__main__'"""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
