6be8e6c8ac0199ef2af6a66e90147be05e3d9d3f
AngelPiva <AngelPiva@gmail.com>
2022-06-02 20:21:57 +0000

    task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,6 +25,7 @@ class Square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
+    """checks if __name__ == "__main__""""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
